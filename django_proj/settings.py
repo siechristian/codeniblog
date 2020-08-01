90485c1029612fd6b3c0424e152b49f5d75ffb21
siechristian <xristianty@gmail.com>
2020-08-01 16:25:06 -0400

    reenable debug. turned off on heroku

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['codeniblog.herokuapp.com/', 'codenificentbg-cli.herokuapp.com', 'codenificentblog.herokuapp.com/', 'tioye.life']
 
 
