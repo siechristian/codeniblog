4656d2e7cd886bee8d8a13905f4922ad69a8f102
siechristian <xristianty@gmail.com>
2020-08-01 03:23:48 -0400

    fix life

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['codeniblog.herokuapp.com/', 'codenificentbg-cli.herokuapp.com', 'codenificentblog.herokuapp.com/', 'tioye.dev']
+ALLOWED_HOSTS = ['codeniblog.herokuapp.com/', 'codenificentbg-cli.herokuapp.com', 'codenificentblog.herokuapp.com/', 'tioye.life']
 
 
 # Application definition
