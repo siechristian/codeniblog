259164f0a86e7e7a86db52eed6ff4a4f16a49563
CodenificenT <61930093+siechristian@users.noreply.github.com>
2021-04-19 06:29:27 -0400

    Update settings.py

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -122,21 +122,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/profile_pics/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/profile_pics')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
