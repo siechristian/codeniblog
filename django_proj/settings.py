12b72c524c6026246d9373a842c238ec2cf0e41f
CodenificenT <61930093+siechristian@users.noreply.github.com>
2021-04-19 06:14:48 -0400

    NEW UPDATE
    to settings

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages'
+    'storages',
 ]
 
 
@@ -153,6 +153,6 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
